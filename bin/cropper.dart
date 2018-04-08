f7d8623aab69a5dd39c8660c56f1d12cde0d68c0
Oleksandr Kovalchuk <anxolerd@outlook.com>
2018-04-09 00:45:30 +0300

    Read pano from commandline args
    - read pano path from commandline args
    - better errors handling

--- bin/cropper.dart
+++ bin/cropper.dart
@@ -19,19 +19,29 @@ bool canCropImage(Image image) {
 }
 
 
-void main() {
-    String imagePath = 'pano.jpg';
+void main(List<String> arguments) {
+    if (arguments.length != 1) {
+        Io.stderr.writeln('Usage:\n\tdart cropper.dart <pano-to-crop>');
+        Io.exit(2);
+    }
+
+    String imagePath = arguments[0];
+    if (!Io.FileSystemEntity.isFileSync(imagePath)) {
+        Io.stderr.writeln('File $imagePath does not exist');
+        Io.exit(2);
+    }
+
     Image image = decodeImage(new Io.File(imagePath).readAsBytesSync());
 
     if (!canCropImage(image)) {
-        print('Cannot crop this image: its width is less then two heights');
-        return;
+        Io.stderr.writeln('To crop pano, its width should be at least twice of its height');
+        Io.exit(2);
     }
 
     List<Image> slices = slice(image);
 
     for (int piece = 0; piece < slices.length; piece++) {
-        new Io.File('pano-${piece}.jpg')
+        new Io.File('${imagePath}.${piece}.jpg')
         ..writeAsBytesSync(encodeJpg(slices[piece]));
     }
 }
